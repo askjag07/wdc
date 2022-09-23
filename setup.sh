ee9ffe6778fe3619245026814995bbadf8b73237
askjag07 <askjag07@gmail.com>
2022-09-22 21:26:14 -0400

    Updated README.md and env setup instructions.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
+######### MacOS only. #########
+
+cd Desktop/rapid-jamplate
 touch $HOME/.zshrc
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install --lts
 nvm use --lts
-echo "\n\nYou're welcome :)"
+npm i -g npm npm-check-updates gatsby-cli
+npm i
+echo "\n\nYou're welcome ;)"
